007a47e55ee2eaf9b4b09a6d52aa1f2b7dbfa3af
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2024-07-02 15:22:22 -0300

    Refactor translations module
    It was an attempt to get the proper translations paths, but due to Vite
    shortcomings, that differently than Node.js `import()`, doesn't accepts
    functions nor variables to define the import path.
    
    So the template string must begin with "./" or "../" for Vite to
    correctly make the import.
    
    This modifies the translation module to become a locale filter to
    provide only the locales we support on our application.

--- app/lib/translations.ts
+++ app/lib/translations.ts
@@ -1,12 +1,5 @@
-export type SupportedTranslations = "en" | "es"
+export const supportedTranslations = [ "en", "es" ]
 
-export const translations: SupportedTranslations[] = [ "en", "es" ]
-
-// TODO use node:path.join() and move this to an `only.server` folder, this is
-// not a frontend module
-// Also, maybe I cannot use this, as Vite needs the dynamic import to start with
-// "./" or "../"
-export const getTranslationPath = (language: SupportedTranslations, section: string): string => {
-  // We'll try to match `section` to route names, but you can choose any name
-  return `${translations[language]}${section}.js`
-}
+export const getSupportedLocale = (locale: string): string =>
+  // fallback to English if we don't support this locale
+  supportedTranslations.includes(locale) ? locale : "en"
