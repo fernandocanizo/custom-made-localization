63d78bcdb4f1fca6abe150c61bfd81419160e113
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2024-07-02 10:37:52 -0300

    WIP: library changes to make it work
    Need to work more on this now that I got ride of Vite/Rollup errors and
    know how to properly make dynamic imports. But this is a demo repo, so
    I'll do it later.

--- app/lib/translations.ts
+++ app/lib/translations.ts
@@ -1,12 +1,11 @@
 export type SupportedTranslations = "en" | "es"
 
-export const translations: Record<SupportedTranslations, string> = {
-  en: "~/translations/en/",
-  es: "~/translations/es/",
-}
+export const translations: SupportedTranslations[] = [ "en", "es" ]
 
-// TODO should use node:path.join(), but I'm not sure what would it bring into
-// the frontend JS. So first test the hypothesis and then deal with that issue
+// TODO use node:path.join() and move this to an `only.server` folder, this is
+// not a frontend module
+// Also, maybe I cannot use this, as Vite needs the dynamic import to start with
+// "./" or "../"
 export const getTranslationPath = (language: SupportedTranslations, section: string): string => {
   // We'll try to match `section` to route names, but you can choose any name
   return `${translations[language]}${section}.js`
